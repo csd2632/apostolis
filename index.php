594ede1024bd41f540b60c32654eb6f966a8aaf7
nikolaou_ap <nikolaou_ap@ymail.com>
2021-09-19 11:31:59 +0300

    login button

--- index.php
+++ index.php
@@ -137,7 +137,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </ul>
         <ul class="nav navbar-nav navbar-right">
           <li><button class="btn btn-muted navbar-btn" id="signUpButton" data-toggle="modal" data-target="#registerPopUp"><span class="glyphicon glyphicon-user"></span> Sign Up</button></li>
-          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          <li><button class="btn btn-muted navbar-btn" id="signUpButton" data-toggle="modal" data-target="#exampleModal"><span class="glyphicon glyphicon-user"></span> Login</button></li>
+          
         </ul>
 
       </div>
@@ -222,6 +223,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       </div>
     </div>
   </div>
+
+  <!-- modal2 -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 
 </html>
